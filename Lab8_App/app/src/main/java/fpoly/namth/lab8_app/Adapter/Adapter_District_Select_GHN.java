078115586e26db4e2fe2aa1f45c8736ccc84c1d9
namth1711 <trinhnam877@gmail.com>
2024-04-08 07:46:25 +0700

    Lab8

---
+++ Lab8_App/app/src/main/java/fpoly/namth/lab8_app/Adapter/Adapter_District_Select_GHN.java
@@ -0,0 +1,54 @@
+package fpoly.namth.lab8_app.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import fpoly.namth.lab8_app.Models.District;
+import fpoly.namth.lab8_app.Models.Province;
+import fpoly.namth.lab8_app.R;
+
+import java.util.ArrayList;
+
+public class Adapter_District_Select_GHN extends ArrayAdapter<District> {
+    private Context context;
+    private ArrayList<District> list;
+
+    public Adapter_District_Select_GHN(@NonNull Context context, ArrayList<District> list) {
+        super(context, 0, list);
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return initView(position, convertView, parent);
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return initView(position, convertView, parent);
+    }
+
+    private View initView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_district_spinner, parent, false);
+        }
+
+        TextView textViewProvinceName = convertView.findViewById(R.id.textViewDistrictName);
+
+        District currentItem = getItem(position);
+
+        if (currentItem != null) {
+            textViewProvinceName.setText(currentItem.getDistrictName());
+        }
+        return convertView;
+    }
+}
